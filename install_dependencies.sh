e1c389f2b9ff24fbbe384ef7e3632f9d415b9d15
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2025-08-20 20:05:50 +0200

    [main][fix] fix 3 dep

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -14,18 +14,5 @@ fi
 
 export CUDA_INSTALLED=$cuda_installed
 
-# install dependencies based on CUDA installation
-if [ $cuda_installed -eq 1 ]; then
-    uv sync --extra gpu
-    echo "UV sync with GPU support"
-
-    # pip install flash-attn --no-build-isolation
-    # echo "Flash attention installed"
-
-    # export FAISS_ENABLE_GPU=ON FAISS_OPT_LEVEL=avx512 FAISS_ENABLE_CUVS=ON
-    # pip install --no-binary :all: faiss-cpu
-    # echo "FAISS with GPU support installed"
-else
-    uv sync
-    echo "UV sync without GPU support"
-fi
+# install dependencies 
+uv sync
